5adcabb9ba3090918332b477986ccdb6b61e75dd
Udaykiranakk <144661563+Udaykiranakk@users.noreply.github.com>
2024-03-10 17:08:16 +0530

    created beer react app with search and cards,hover effects

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [beers, setBeers] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("https://api.punkapi.com/v2/beers")
+      .then((response) => {
+        setBeers(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredBeers = beers.filter((beer) =>
+    beer.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="title">Beer Catalog</h1>
+      <input
+        type="text"
+        placeholder="Search..."
+        value={searchTerm}
+        onChange={handleSearch}
+      />
+      <div className="beer-container">
+        {filteredBeers.map((beer) => (
+          <div key={beer.id} className="beer-card">
+            <img src={beer.image_url} alt={beer.name} />
+            <div>
+              <h2>{beer.name}</h2>
+              <p>{beer.tagline}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
